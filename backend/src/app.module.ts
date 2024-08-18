963432d193a62185dd0fde8143ed5643a89e1da6
poonyawat0931 <117950023+poonyawat0511@users.noreply.github.com>
2024-08-19 02:00:03 +0700

    setup and add dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './app/config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+import { MulterModule } from '@nestjs/platform-express';
+import { storageConfig } from './app/config/storage.config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('mongodb.uri'),
+        dbName: config.get<string>('mongodb.dbName'),
+      }),
+    }),
+    ServeStaticModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          rootPath: join(__dirname, '..', 'uploads'),
+          serveRoot: config.get('upload.apiPath'),
+        },
+      ],
+    }),
+    MulterModule.register({ storage: storageConfig }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import * as compression from 'compression';
 
 async function bootstrap() {
+  // app settings
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.enableCors({ origin: '*' });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // config
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get<number>('port');
+
+  // Express middleware
+  app.use(compression());
+  app.use(morgan('dev'));
+
+  await app.listen(port);
 }
 bootstrap();
